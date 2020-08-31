c175473b5072ed541960a9cf5480a981c753b095
Michael Byalsky <michael.byalsky@gmail.com>
2020-08-31 12:18:30 +0300

    uibuild

---
+++ build/precache-manifest.cc6712c145795ce088fcf2e9d4f3e9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271242a376be761c49768c0efa55c0db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c845dee2475c56d8e80",
+    "url": "/static/css/main.e52c53ec.chunk.css"
+  },
+  {
+    "revision": "42a80b7a79774f8baaa9",
+    "url": "/static/js/2.7b419685.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7b419685.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c845dee2475c56d8e80",
+    "url": "/static/js/main.b02b0539.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
